5fdbd7310e14f451e27e5ebdff78f73e029dd53a
Siradji <siradjiawoual@gmail.com>
2021-04-17 00:59:30 +0100

    Setup testing infrustructure, and styling

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Javascript development blog - Suraj Auwal </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-main-red">Hello world</h1>
+      </main>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      'main-red': "#ff4a57",
+      'main-white': "#fefefe", 
+      'main-blue': "#1f2235",
+      'soft-blue': "#23263a"
+    }
   },
   variants: {
     extend: {},
