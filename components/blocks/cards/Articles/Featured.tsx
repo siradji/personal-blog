7e1293045a0f25ba6e9457e8a7feae616062b23f
Siradji <siradjiawoual@gmail.com>
2021-04-20 01:27:34 +0100

    Opt for staging

---
+++ components/blocks/cards/Articles/Featured.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Featured = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ components/blocks/cards/Articles/GeneralArticle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GeneralArticle = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GeneralArticle
